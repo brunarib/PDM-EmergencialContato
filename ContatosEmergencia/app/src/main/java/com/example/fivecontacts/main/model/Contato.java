5a403ba934a80bf800dedcd1de96cab680e1f384
bruna ribeiro <m.bruna.ribeiro.o@gmail.com>
2021-08-02 10:15:56 -0300

    first commit

---
+++ ContatosEmergencia/app/src/main/java/com/example/fivecontacts/main/model/Contato.java
@@ -0,0 +1,32 @@
+package com.example.fivecontacts.main.model;
+
+import android.util.Log;
+
+import java.io.Serializable;
+
+public class Contato implements Serializable, Comparable {
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    String nome;
+    String numero;
+
+    @Override
+    public int compareTo(Object o) {
+        Contato c2= (Contato)o;
+        return this.getNome().compareTo(c2.getNome());
+    }
+}
